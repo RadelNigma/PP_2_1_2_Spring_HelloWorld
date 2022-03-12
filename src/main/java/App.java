3ddc9eb480182c075dc2c365e862be97f30c8fe9
RadelNigma <radelnigma@yandex.ru>
2022-03-13 01:27:52 +0300

    Final

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getName() + " " + bean3.getAge());
+
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean4.getName() + " " + bean4.getAge());
+
+        System.out.println("HelloWorld bean==bean2 " +  (bean==bean2));
+        System.out.println("Cat bean3==bean4 " +  (bean3==bean4));
     }
 }
